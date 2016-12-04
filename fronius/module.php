a4c7974c9ca67e1a703d045848f2c0fc2768b4e9
stuzi01 <stefan@ploecking.de>
2016-12-04 16:58:20 +0100

    Update module.php

--- fronius/module.php
+++ fronius/module.php
@@ -1,7 +1,29 @@
-
 <?
-{ // Klassendefinition
+    // Klassendefinition
     class Fronius extends IPSModule {
+ 
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+ 
+            // Selbsterstellter Code
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+ 
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+ 
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
@@ -9,20 +31,9 @@
         * ABC_MeineErsteEigeneFunktion($id);
         *
         */
-        
-    private $client;
-    private $tokens;     	
-    private $refresh_token;
-    private $access_token;
-    private $deviceList;
-    private $echoString;
-        
-        
-        
         public function MeineErsteEigeneFunktion() {
-            echo $this->InstanceID;
+            // Selbsterstellter Code
         }
     }
-}
 ?>
 
